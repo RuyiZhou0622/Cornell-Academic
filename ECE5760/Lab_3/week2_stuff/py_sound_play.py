8b4538cde9f1f1fd78c0516e4fdf61eb30ec816f
RUYI ZHOU <70717006+RuyiZhou0622@users.noreply.github.com>
2024-03-20 15:08:56 -0400

    Update py_sound_play.py

--- ECE5760/Lab_3/week2_stuff/py_sound_play.py
+++ ECE5760/Lab_3/week2_stuff/py_sound_play.py
@@ -1,8 +1,20 @@
+#!/usr/bin/env python3
+# =========================================================================
+# parse.py
+# =========================================================================
+# Code for turning ModelSim output into a .wav sound file
+#
+# Author: Aidan McNay
+# Date: March 18th, 2024
+
 import soundfile as sf
 import numpy as np
 
+# ------------------------------------------------------------------------
 # Read the data
-with open("output_w2.txt") as f:
+# ------------------------------------------------------------------------
+
+with open("output_w2_nonlinear.txt") as f:
     lines = f.readlines()
     data = []
     for line in lines:
@@ -12,19 +24,16 @@ with open("output_w2.txt") as f:
         except (IndexError, ValueError):
             continue # Probably just not a line corresponding to real data
 
+# ------------------------------------------------------------------------
 # Normalize
+# ------------------------------------------------------------------------
+
 data = np.asarray(data)
 data = data / np.max(np.abs(data))
 
-# Extend the data to 1 second
-desired_length = 48000  # 1 second at 48000 Hz sample rate
-current_length = len(data)
-remainder = desired_length - current_length
-if remainder > 0:
-    extended_data = np.concatenate((data, np.zeros(remainder)))
-else:
-    extended_data = data[:desired_length]  # Truncate if the data is already longer than 1 second
-
+# ------------------------------------------------------------------------
 # Convert to .wav file
-sf.write("drum_w2_extended.wav", extended_data, 48000)
-print("DONE!")
+# ------------------------------------------------------------------------
+
+sf.write("drum_nonlinear.wav", data, 48000)
+print("DONE !")
